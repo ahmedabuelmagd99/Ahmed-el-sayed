ada69046d0771470f69531ac5dc5bf462652d5ad
ahmedabuelmagd99 <83430423+ahmedabuelmagd99@users.noreply.github.com>
2021-04-29 23:52:55 +0200

    Add files via upload

---
+++ Ahmed el sayed.sh
@@ -0,0 +1,42 @@
+mkdir TEST
+mkdir -p TEST/TEST1
+mkdir -p TEST/TEST2
+
+cd TEST
+cd TEST/TEST1
+cat > file1.txt
+Ahmed el sayed 
+cat > file2.txt
+hello world 
+cat > file3.txt
+hello
+cd .. 
+cd ..
+
+cd TEST
+cd TEST/TEST2
+
+cd ..
+cd ..
+
+cat > Ahmed.txt
+cat > ahmed.txt
+
+ls a* A*
+ahmed.txt  Ahmed.txt
+
+ls -1 |  wc -l
+11
+date 
+wed 28 April 2021 04:42:20 AM PDT 
+ls 
+ahmed.txt Desktop Downloads pictures Templates Videos 
+Ahmed.txt Documents Music Public 
+pwd 
+/home/ubuntu
+cat > Ahmed.txt
+my name is ahmed el sayed. 
+wc ahmed.txt
+4 4 31 ahmed.txt 
+cp /HOME/TEST/TEST1/file1.txt  /HOME/TEST/TEST2
+
